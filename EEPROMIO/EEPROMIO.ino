3c932114ad2c1c6e222bc774be1b8f921bd23074
James Alexander Hughes <jameshughes89@Gmail.com>
2022-12-23 11:34:09 -0400

    read serial function

--- EEPROMIO/EEPROMIO.ino
+++ EEPROMIO/EEPROMIO.ino
@@ -41,13 +41,37 @@ void setAddress(int address, bool outputEnable) {
 // Read and return the data from the specified memory address. Each data (word) is 8 bits (1 byte). 
 //
 // int address: Address to read from 
-byte readEEPROM(int address) {
+byte readEepromAddress(int address) {
   setAddress(address, /*outputEnable*/ true);
   byte word = 0;
   for(int pin = EEPROM_DATA_7; pin >= EEPROM_DATA_0; pin--) {
-    word (data << 1) + digitalRead(pin);
+    word = (word << 1) + digitalRead(pin);
   }
   return word;
+}
+
+
+// Read the contents of the EEPROM up to the specified address. The contents of the EEPROM are 
+// written to the serial output. This assumes that pins TX & RX are not used since the serial
+// interface requires them to be open. This assumes the upToAddress is a multiple of 16. If the 
+// specified address is not a multiple of 16 it is rounded down to the nearest multiple of 16.
+//
+// int upToAddress: Address to read up to. If it is not a multiple of 16 it is rounded down.
+void readEepromSerial(int upToAddress) {
+  // Loop to the rounded down version of the specified address
+  for(int base = 0; base < (upToAddress - (upToAddress % 16)); base += 16) {
+    byte words[16];
+    for(int offset = 0; offset < 16; offset++) {
+      words[offset] = readEepromAddress(base + offset);
+    }
+    char outputBuffer[80];
+    sprintf(outputBuffer, 
+      "%03x:\t%02x %02x %02x %02x  %02x %02x %02x %02x    %02x %02x %02x %02x  %02x %02x %02x %02x",
+      base, 
+      words[0], words[1], words[2], words[3], words[4], words[5], words[6], words[7], 
+      words[8], words[9], words[10], words[11], words[12], words[13], words[14], words[15]);
+      Serial.println(outputBuffer);
+  }
 }
 
 // put your setup code here, to run once:
@@ -55,6 +79,10 @@ void setup() {
   pinMode(SHIFT_REGISTER_DATA, OUTPUT);
   pinMode(SHIFT_REGISTER_CLOCK, OUTPUT);
   pinMode(SHIFT_REGISTER_LATCH, OUTPUT);
+
+  Serial.begin(57600);
+  readEepromSerial(256); 
+  Serial.end();
 }
 
 // put your main code here, to run repeatedly:
